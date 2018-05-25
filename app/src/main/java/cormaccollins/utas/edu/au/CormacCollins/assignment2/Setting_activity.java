777df4e14622dc0c764f611f4735f1114ef20748
CormacCollins <100655400@student.swin.edu.au>
2018-05-25 15:47:51 +1000

    Updated combined work

--- app/src/main/java/cormaccollins/utas/edu/au/CormacCollins/assignment2/Setting_activity.java
+++ app/src/main/java/cormaccollins/utas/edu/au/CormacCollins/assignment2/Setting_activity.java
@@ -1,9 +1,18 @@
 package cormaccollins.utas.edu.au.CormacCollins.assignment2;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -16,5 +25,61 @@ public class Setting_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_layout);
 
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
+        builder.setTitle("List Name:");
+
+        final SearchView srchView = findViewById(R.id.searchView);
+        srchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                createAlertDialog(srchView);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // do something when text changes
+                return false;
+            }
+        });
+
+
+
+
+
+    }
+
+    private void createAlertDialog(final View view){
+
+
+        //https://stackoverflow.com/questions/10903754/input-text-dialog-android
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setTitle("List Name:");
+
+        final EditText input = new EditText(view.getContext());
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+
+        builder.setPositiveButton("Create List", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String s = input.getText().toString();
+
+//                Intent i = new Intent(view.getContext(), ListActivity.class);
+//                i.putExtra("ListName", s);
+//                startActivity(i);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
